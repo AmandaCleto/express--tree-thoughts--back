a3f26e6b20383492feca4972637562388e578c01
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-06-15 22:19:04 -0300

    Implementing cors middleware and adding static route

--- src/server/index.js
+++ src/server/index.js
@@ -4,12 +4,18 @@ const express = require('express');
 const http = require('http');
 const process = require('process');
 const router = require('../routes/index.routes');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use('/static', express.static(publicPath));
 
 const PORT = process.env.PORT;
 
