0dcf3ecdc81d407fe7eb1e4d18b2e29af39cecf5
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-06 22:26:29 -0300

    adding sequelize config file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
-const process = require('process');
+require('dotenv').config();
+
 const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
+    database:  DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'mysql',
