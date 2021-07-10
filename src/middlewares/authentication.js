3f3e4e41f996b22e6dfdb6bc7dda2c68da85ff3c
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-09 22:53:16 -0300

    removing tests

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,20 +1,5 @@
 function authMiddleware(req, res, next) {
-    const { username, password } = req.body;
-    
-    const database = {
-        caio: {
-            password: 123
-        }
-    }
-
-    if(database[username]?.password === password) {
-        return next();
-    }else {
-        res.status(401).json({
-            authenticated: false
-        })
-        return false;
-    }
+    return next();
 }
 
 module.exports = {
