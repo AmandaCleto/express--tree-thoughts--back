42ae6fccbc35f112fd2989aa8b17946b0fc85afb
AmandaCleto <amanda.clet@gmail.com>
2021-07-19 23:02:13 -0300

    renaming model users to user; creating hash for password; deleting return of password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,16 +1,19 @@
-const Users = require('../models/users');
+const User = require('../models/user');
 
 async function create(req, res) {
     try {
         const { name, email, password } = req.body;
 
-        const userCreated = await Users.create({
+        const userCreated = await User.create({
             name,
             email,
             password
         });
 
+        delete userCreated.password;
+
         return res.json({userCreated});
+
     } catch ({errors}) {
         const { message, path } = errors[0];
         return res.status(400).json({
--- src/models/user.js
+++ src/models/user.js
@@ -3,15 +3,16 @@
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../database/connection');
 
+const bcrypt = require('bcryptjs');
 
-class Users extends Model {
+class User extends Model {
 
   static associate(models) {
     // define association here
   }
 };
 
-Users.init({
+User.init({
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true
@@ -37,18 +38,17 @@ Users.init({
     },
   },
   password: DataTypes.CHAR(60),
-  profile_name: DataTypes.STRING,
+  profile_image: DataTypes.STRING,
   create_at: DataTypes.DATE,
   update_at: DataTypes.DATE,
 }, {
   hooks: {
-    beforeValidate: (User, options) => {
-      // User.changed criptografar senha
-      // console.log(User, options);
+    afterValidate: (User) => {
+      User.password = bcrypt.hashSync(User.password, 10);
     }
   },
   sequelize,
   modelName: 'Users',
 });
 
-module.exports = Users;
+module.exports = User;
