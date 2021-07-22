ba9c2ebdc1af7080e5e4ca227d8f36736d4fbe52
AmandaCleto <amanda.clet@gmail.com>
2021-07-21 22:59:57 -0300

    creating new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 const { Router } = require('express');
 const router = Router();
+
 const { authMiddleware } = require('../middlewares/authentication');
+
 const userController = require('../controllers/user');
 const authenticateController = require('../controllers/authenticate');
+const thoughtController = require('../controllers/thought');
 
-router.post('/user', userController.create);
-router.post('/authenticate', authenticateController.authenticate);
+router.post('/authenticate', authenticateController.authenticate); // login
+
+router.post('/user', userController.create); // signin - register
+
+router.post('/thought', [authMiddleware], thoughtController.create); // register new thought
+
+router.get('/thought', [authMiddleware], thoughtController.get); // get list of thoughts
+
+router.delete('/thought/:thought_id', [authMiddleware], thoughtController.destroy); // get list of thoughts
 
 module.exports = router;
